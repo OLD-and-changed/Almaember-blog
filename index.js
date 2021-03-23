e2b59754b86c33a65538d9165c938a9c9e8c0397
Almaember <almaember@protonmail.com>
2021-03-23 16:21:15 +0100

    A few updates, fix blog.css

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const {compile} = require("handlebars");
+const fs = require("fs");
 
+const {compileFile} = require("handlebars");
+
+let template = compileFile("page.handlebars");
+
+// prepare the output folder
